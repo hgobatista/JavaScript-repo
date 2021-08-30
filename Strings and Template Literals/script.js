6bb8017137f00f1f447698d2d2f8aa4a01b39469
hgobatista <74008353+hgobatista@users.noreply.github.com>
2021-08-30 01:44:14 -0300

    Add files via upload

---
+++ Strings and Template Literals/script.js
@@ -0,0 +1,11 @@
+const firstName = 'Hugo';
+const age = 26;
+const job = 'Designer';
+
+const hugo = `I'm ${firstName}, a ${age} years old ${job}`;
+console.log(hugo);
+
+//Como pular linhas (também podemos usar \n\, porém é recomendado a forma abaixo)
+console.log(`String
+multiple
+lines`);
