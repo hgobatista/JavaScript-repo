71bf21c994b2a1f40ea27ece4a03b18889539d31
hgobatista <74008353+hgobatista@users.noreply.github.com>
2021-09-03 16:29:24 -0300

    Add files via upload

---
+++ Loop - Intro/script.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 1');
+*/
+
+// for loop keeps running while condition is TRUE
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+};
