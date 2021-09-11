6e9466d074eea6f9709c3b6f8498e1330c867d58
hgobatista <74008353+hgobatista@users.noreply.github.com>
2021-09-10 23:38:04 -0300

    Update script.js

--- Array - Destructuring/script.js
+++ Array - Destructuring/script.js
@@ -2,7 +2,7 @@
 
 const restaurant = {
   name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  location: "Via angelo, Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
@@ -11,39 +11,74 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
-
+// Original Array
 const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// Desestruturação NEWBIE:
+const one = arr[0];
+const two = arr[1];
+const three = arr[2];
 
-const [x, y, z] = arr; // Parece um array, mas é um desestruturador de array
-console.log(x, y, z);
-console.log(arr);
+// Desestruturando o array PRO (o array original não foi modificado);
+const [a, b, c] = arr;
+console.log(a, b, c);
 
-let [main, , secondary] = restaurant.categories; // Para pular um, basta deixar um espaço em branco
-console.log(main, secondary);
+// Vamos retirar alguns elementos do array 'categories'
+// Note que não retiramos todos os elementos, apenas o primeiro e o segundo
+let [first, second] = restaurant.categories;
+console.log(first, second);
 
-// Switching variables
-/*const temp = main;
-main = secondary;
-secondary = temp;
-console.log(main, secondary);
+// Agora, digamos que queremos retirar apenas o primeiro e o
+// terceiro item... Faremos então desta forma:
+const [first1, , third] = restaurant.categories;
+console.log(first1, third); // Note que deixamos um espaço entre vírgulas para isso
+
+// Digamos que o dono do restaurante queira mudar a ordem
+// da primeira para segunda categoria (era Italian, Pizzeria)
+/* Modo newbie
+const temp = first;
+first = second;
+second = temp;
+console.log(first, second);
 */
+// Modo PRO
+[first, second] = [second, first];
+console.log(first, second);
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+/* Caso tenhamos uma função retornando um array, podemos
+desestruturar o resultado, retornando multiplos resultados*/
+const [starter, main] = restaurant.order(2, 0);
+// Podemos retornar o array:
+console.log(restaurant.order(2, 0));
+// Ou retornar os itens separados:
+console.log(starter, main);
 
-// Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
-
-// Nested destructuring
+// Se tivermos um 'Nested array', um array dentro do outro
 const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// Digamos que queremos o primeiro item e o array inteiro:
+const [arr1, , arr3] = nested;
+console.log(arr1, arr3);
+// Digamos ainda que queremos o primeiro
+// item e os dois items do array:
+/* Jeito newbie:
+const [arrA, arrB] = arr3;
+console.log(arr1, arrA, arrB);
+*/
+//Jeito pro:
+/* const [arr1, , [arr4, arr5]] = nested;
+   console.log(arr1, arr4, arr5);
+*/
+//-------------------------
+/*
+  Podemos setar default values para variáveis que
+  vamos desestruturar, caso nao aja resultado.
 
-// Default values
-const [p = 1, q = 1, r = 1] = [8, 9];
+  Digamos que temos dois numeros, 8 e 9, mas não sabemos
+  disso e tentamos buscar três números no array. Desta forma,
+  setamos um valor default.
+*/
+const [p, q, r] = [8, 9];
 console.log(p, q, r);
+// Obteremos assim um valor 'undefined'
+// Assim, colocamos um valor default
+const [m = 1, n = 1, o = 1] = [8, 9];
+console.log(m, n, o);
