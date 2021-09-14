f2d3819aa300c1a0c968d4fce2229ac27debafa4
hgobatista <74008353+hgobatista@users.noreply.github.com>
2021-09-14 15:06:50 -0300

    Add files via upload

---
+++ Challenge 11 - Data Structures and Modern Operators copy/script.js
@@ -0,0 +1,62 @@
+/* Coding Challenge 11
+
+Let's continue with our football betting app! This
+time, we have a map with a log of the events that
+happened during the game. The values are the events
+themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes
+plus some extra time).
+
+1. Create an array 'events' of the different game
+events that happened (no duplicates);
+2. After the game has finished, is was found that the
+yellow card from minute 64 has unfair. So remove this
+event from the game events log;
+3. Print the following string to the console: "An
+event happened, on average, every 9 minutes" (keep in
+mind that a game has 90 minutes);
+4. Loop over the events and log them to the console,
+marking whether it's in the first half or second half
+(after 45min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+
+*/
+"Use strict";
+const gameEvents = new Map([
+  [17, "⚽  GOAL"],
+  [36, "🔁  Substitution"],
+  [47, "⚽  GOAL"],
+  [61, "🔁  Substitution"],
+  [64, "🔸  Yellow card"],
+  [69, "🔴  Red card"],
+  [70, "🔁  Substitution"],
+  [72, "🔁  Substitution"],
+  [76, "⚽  GOAL"],
+  [80, "⚽  GOAL"],
+  [92, "🔸  Yellow card"],
+]);
+
+// 1. Task
+const events = [...gameEvents.values()];
+const eventsSet = [...new Set(events)];
+console.log(eventsSet);
+
+// 2. Task
+gameEvents.delete(69);
+console.log(gameEvents);
+
+// 3. Task
+console.log(
+  `An event happened, on average, every ${92 / gameEvents.size} minutes.`
+);
+
+// 4. Task
+for (const [minute, event] of gameEvents) {
+  let half;
+  if (minute <= 45) {
+    half = "FIRST";
+  } else {
+    half = "SECOND";
+  }
+  console.log(`[${half} HALF] ${minute}: ${event}`);
+}
